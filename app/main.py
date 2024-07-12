1787cf6428430da48e5ab53c3ec882377ad7c194
Krishnamurariyadav123 <126970114+Krishnamurariyadav123@users.noreply.github.com>
2024-07-12 23:18:44 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"title": "Hello Krishnamurai  :)"}
-
-Instrumentator().instrument(app).expose(app)
+    return {
+        "title": "Hello Krishnamurari yadav :)",
+        "message": "We have succesffuly launch  FastAPI application!",
+        "status": "Running smoothly"
+    }
